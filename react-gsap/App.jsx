97282deaf29df664b436bd9406c64070239d922c
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-05-24 14:04:06 +0530

    contextSave

--- react-gsap/App.jsx
+++ react-gsap/App.jsx
@@ -3,29 +3,22 @@ import gsap from 'gsap'
 import React, { useRef, useState } from 'react'
 
 export default function App() {
-  const [xValue, setXValue] = useState(0);
-  const [rotateX, setRotateX] = useState(0);
-  const boxRef = useRef();
-  const randomX = gsap.utils.random(-500, 500, 100);
-  const randomRotate = gsap.utils.random(-360, 360, 30);
+//  if we did not use useGsap then also the animation work but it stick to the memory so if we build a bigger website it takes extra space.
 
+const { contextSafe } = useGSAP();
+const boxRef = useRef();
 
-  useGSAP(() => {
-    gsap.to( boxRef.current, {
-      x: xValue,
-      duration: 1,
-      rotate: rotateX
+  const rotateBox = contextSafe(()=> {
+    gsap.to(boxRef.current, {
+      rotate: 360,
+      duration: 1
     })
-  }, [xValue, rotateX])
-
-  // but if we want to use scope and dependencies then we need to write the last line like this .
-  // }, {scope: "main", dependencies: [xValue, rotateX]});
+  })
 
   return (
     <main>
-      <button onClick={()=>{ setXValue(randomX)
-        setRotateX(randomRotate)}} >Animate</button>
-      <div ref={boxRef} className="box"></div>
+      <button onClick={rotateBox}>Animate</button>
+      <div ref={boxRef} className="box">Box</div>
     </main>
   )
 }
