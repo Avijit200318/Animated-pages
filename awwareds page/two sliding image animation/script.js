4394ea1aa95f860d01fd04dfdbb45f0f9717bab1
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-06-07 13:12:36 +0530

    updated the website

--- awwareds page/two sliding image animation/script.js
+++ awwareds page/two sliding image animation/script.js
@@ -1,35 +1,41 @@
-const tl0 = gsap.timeline({
-    delay: 0.5,
-    duration: 1,
-})
+const tl = gsap.timeline()
 
-tl0.from(".textBox h1", {
+tl.from("nav", {
+    y: -100,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+})
+.from(".textBox h1", {
     y: 200,
     duration: 0.5,
-    stagger: 0.1,
 })
-.from(".text p", {
+.from(".text p, .btn", {
     y: 100,
     opacity: 0,
+    durtion: 0.5,
+    stagger: 0.3
 })
-.from(".text a", {
-    x: -100,
+.from(".lineone", {
+    left: "50%",
     opacity: 0,
+    duration: 0.5,
+    stagger: 0.5,
 })
 
-const tl = gsap.timeline({
+const tl1 = gsap.timeline({
     scrollTrigger: {
         trigger: "#page1",
         start: "50% 50%",
-        end: "150% 50%",
+        end: "200% 50%",
         // markers: true,
         scrub: 2,
         pin: true,
     }
 });
 
-tl.to("#firstline", {
-    marginTop: "-120vh",
+tl1.to("#firstline", {
+    marginTop: "-170vh",
 }, 'variable')
 .to("#secondline", {
     marginTop: "20vh",
