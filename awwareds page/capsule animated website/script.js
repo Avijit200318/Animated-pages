2eda1df3ce8a5c31f03dcf07cd0c5eb4e958c839
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-06-30 10:01:06 +0530

    created full website

--- awwareds page/capsule animated website/script.js
+++ awwareds page/capsule animated website/script.js
@@ -34,7 +34,7 @@ const tl1 = gsap.timeline({
   }
 })
 
-tl1.to(".background img", {
+tl1.to("#bgImage", {
   transform: "scale(1.05)",
 })
 
@@ -70,3 +70,71 @@ tl3.to(".hide", {
   top: "-100%",
   stagger: 0.1
 })
+
+// page4 animation
+const tl4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page4",
+    start: "30% 30%",
+    end: "220% 30%",
+    // markers: true,
+    scrub: 1,
+    pin: true,
+
+    // this two is used to define the ending animation after the scrolltrigger is ended.
+    // onLeave:
+    // onEnterBack:
+  }
+})
+
+
+tl4.to(".box h3", {
+  opacity: 0,
+}, 'a')
+.to(".page4 .background", {
+  width: "calc(100vw - 1rem)",
+  height: "calc(100vh - 1rem)",
+  borderRadius: "3.5rem",
+  y: -40,
+}, 'a')
+.to(".page4 .background img", {
+  transform: "scale(1)",
+}, 'a')
+.from(".background .topText h4, .background .topText h3, .background .bottomText h3", {
+  opacity: 0,
+  x: 50,
+})
+tl4.to({}, { duration: 0.4 }, "+=0")
+
+.to("#second", {
+  transform: "translate(-50%, -56%)",
+}, 'b')
+.to("#second img", {
+  transform: "scale(1)",
+}, 'b')
+.to(".page4 .background", {
+  scale: 0.9,
+  opacity: 0,
+  y: -50
+}, 'b')
+.from(" #second .topText h4, #second .topText h3, #second .bottomText h3", {
+  opacity: 0,
+  x: 50,
+})
+tl4.to({}, { duration: 0.4 }, "+=0")
+
+.to("#third", {
+  transform: "translate(-50%, -56%)",
+}, 'c')
+.to("#third img", {
+  transform: "scale(1)",
+}, 'c')
+.to("#second", {
+  scale: 0.9,
+  opacity: 0,
+}, 'c')
+.from("#third .topText h4, #third .topText h3, #third .bottomText h3", {
+  opacity: 0,
+  x: 50,
+})
+tl4.to({}, { duration: 0.4 }, "+=0")
